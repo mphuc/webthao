0d6a74722d88d55e24ade0518ebb2442c7372bd6
mphuc <mphuc2790@gmail.com>
2016-12-04 00:36:24 +0700

    áds

---
+++ admin/config.php
@@ -0,0 +1,38 @@
+<?php
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost/webthao/admin/');
+define('HTTP_CATALOG', 'http://localhost/webthao/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/webthao/admin/');
+define('HTTPS_CATALOG', 'http://localhost/webthao/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\webthao/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\webthao/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\webthao/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\webthao/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\webthao/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\webthao/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\webthao/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\webthao/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\webthao/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\webthao/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\webthao/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\webthao/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'coinmax');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'sm_');
+
+require_once '../block_io/lib/block_io.php';
+define('key', '523a-30aa-fd22-b9ac');
+define('pin', '');
+define('block_version', '2');
+define('wallet', '3Eaggy4ArUpbki29f5PuyyyXNLrUaVFffZ');
---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+
+	// DIR HOST
+	define('url_cmpay','url_cmpay');
+	define('url_cmexchange','url_cmexchange');
+	define('url_financial','url_financial');
+
+	define('DIR_HOST', 'C:\xampp\htdocs\webthao/');
+	// HTTP
+	define('HTTP_SERVER', 'http://localhost/webthao/');
+	// HTTPS
+	define('HTTPS_SERVER', 'http://localhost/webthao/');
+
+	// DIR
+	define('DIR_APPLICATION', DIR_HOST. 'catalog/');
+	define('DIR_SYSTEM', DIR_HOST. 'system/');
+	define('DIR_LANGUAGE', DIR_HOST. 'catalog/language/');
+	define('DIR_TEMPLATE', DIR_HOST. 'catalog/view/theme/');
+	define('DIR_CONFIG', DIR_HOST. 'system/config/');
+	define('DIR_IMAGE', DIR_HOST. 'image/');
+	define('DIR_CACHE', DIR_HOST. 'system/cache/');
+	define('DIR_DOWNLOAD', DIR_HOST. 'system/download/');
+	define('DIR_UPLOAD', DIR_HOST. 'system/upload/');
+	define('DIR_MODIFICATION', DIR_HOST. 'system/modification/');
+	define('DIR_LOGS', DIR_HOST. 'system/logs/');
+
+	// DB
+	define('DB_DRIVER', 'mysqli');
+	define('DB_HOSTNAME', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_DATABASE', 'coinmax');
+	define('DB_PORT', '3306');
+	define('DB_PREFIX', 'sm_');
+
+	// sms
+	
+
+	require_once 'block_io/lib/block_io.php';
+
+	// INVESMENT
+	define('key', '1c8d-162b-1d84-4fe0');
+	define('pin', 'XiKPHb1AvJeaJ1F3pGw6gr11dDt23432412423LJJAN');
+	define('block_version', '2');
+	define('wallet', '38Lg6yUsiEPaHDh33DLxeULnbpexSsm89E');
+	
+	
