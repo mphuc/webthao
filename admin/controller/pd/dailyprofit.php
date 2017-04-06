e4683d1e45b3efe4997a152588e7c344aa66d33f
Your Name <mphuc2790@gmail.com>
2017-04-06 13:07:38 +0700

    a

--- admin/controller/pd/dailyprofit.php
+++ admin/controller/pd/dailyprofit.php
@@ -1,20 +1,28 @@
 <?php
 class ControllerPdDailyprofit extends Controller {
 	public function index() {
-		// Turn off error reporting
-		error_reporting(0);
+		// Turn off all error reporting
+error_reporting(0);
 
-		// Report runtime errors
-		error_reporting(E_ERROR | E_WARNING | E_PARSE);
+// Report simple running errors
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-		// Report all errors
-		error_reporting(E_ALL);
+// Reporting E_NOTICE can be good too (to report uninitialized
+// variables or catch variable name misspellings ...)
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
-		// Same as error_reporting(E_ALL);
-		ini_set("error_reporting", E_ALL);
+// Report all errors except E_NOTICE
+// This is the default value set in php.ini
+error_reporting(E_ALL ^ E_NOTICE);
 
-		// Report all errors except E_NOTICE
-		error_reporting(E_ALL & ~E_NOTICE);
+// Report all PHP errors (see changelog)
+error_reporting(E_ALL);
+
+// Report all PHP errors
+error_reporting(-1);
+
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
 		
 		$this->document->setTitle('Deposit');
 		$this->load->model('pd/registercustom');
