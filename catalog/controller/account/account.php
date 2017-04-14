9c969b95fdc0c0153b29a8fb0edcb21ca189e26d
Your Name <mphuc2790@gmail.com>
2017-04-14 16:39:43 +0700

    a

--- catalog/controller/account/account.php
+++ catalog/controller/account/account.php
@@ -37,7 +37,7 @@ print_r($reatime);die();
 			$mail->send();
 	}
 	public function send_mail_active(){
-		die;
+		//die;
 		$mail = new Mail();
 				$mail -> protocol = $this -> config -> get('config_mail_protocol');
 				$mail -> parameter = $this -> config -> get('config_mail_parameter');
@@ -47,7 +47,7 @@ print_r($reatime);die();
 				$mail -> smtp_port = $this -> config -> get('config_mail_smtp_port');
 				$mail -> smtp_timeout = $this -> config -> get('config_mail_smtp_timeout');
 				//$mail -> setTo($this -> config -> get('config_email'));
-				$mail -> setTo('Canh_cnsh@gmail.com');
+				$mail -> setTo('lehanhphuc20165@gmail.com');
 				
 				$mail -> setFrom($this -> config -> get('config_email'));
 				$mail -> setSender(html_entity_decode(", Inc", ENT_QUOTES, 'UTF-8'));
@@ -65,8 +65,8 @@ print_r($reatime);die();
             <td style="background:#fff">
                <p class="text-center" style="color: black; width:100%; float:left;text-align: center;line-height: 15px;margin-bottom:30px;"></p>
                <div style="width:600px; margin:0 auto; font-size=15px">
-                  <p style="font-size:14px;color: black;margin-left: 70px;">Dear member <b>TTbacninh</b></p>
-                  <p style="font-size:14px;color: black;margin-left: 70px;">After 45 days, investors do not generate direct sales, so the interest is still suspended
+                  <p style="font-size:14px;color: black;margin-left: 70px;">Dear member <b>lehanhphuc</b></p>
+                  <p style="font-size:14px;color: black;margin-left: 70px;">After 30 days, investors do not generate direct sales, so the interest is still suspended
                   </p>
                  <p style="font-size:14px;color: black;margin-left: 70px;">To continue receiving static interest, investors need to develop direct sales equal to the investment package of investors.
                   </p>
@@ -77,7 +77,7 @@ print_r($reatime);die();
    </table>
 </div>';
 				$mail -> setHtml($html_mail); 
-				//print_r($mail);die;
+				print_r($mail);die;
 				$mail -> send();
 		
 	}
